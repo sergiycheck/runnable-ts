97836e61218ddf8f590d5c5bf41864ff55a104c0
serhii <sergiychech@gmail.com>
2022-07-31 20:44:06 +0300

    quick and easy js solution with explanation from wallenben

--- src/index.ts
+++ src/index.ts
@@ -3,52 +3,40 @@ console.log('should run');
 class LRUCache {
   private _capacity = 0;
   private _cache: Map<number, number>;
-  private _lastKey: number;
-  private keysHist: number[];
 
   constructor(capacity: number) {
     this._capacity = capacity;
     this._cache = new Map();
-    this.keysHist = [];
   }
 
   get(key: number): number {
     let result: number;
     if (this._cache.has(key)) {
+      // when we call the 'get' method, push the key to
+      // the end of the queue, as we just accessed it.
       result = this._cache.get(key);
+
+      this._cache.delete(key);
+      this._cache.set(key, result);
     } else {
       result = -1;
     }
-
-    this._lastKey = key;
-    this.keysHist.push(this._lastKey);
 
     return result;
   }
 
   put(key: number, value: number) {
+    const cacheContainsKey = this._cache.has(key);
     const exceedsCapacity = this._cache.size + 1 > this._capacity;
 
-    if (exceedsCapacity) {
-      if (this._cache.size === 1) {
-        const [first] = this._cache.keys();
-        this._cache.delete(first);
-      } else {
-        let reversedKeysHist = this.keysHist.slice().reverse();
-        let indexOfReversedLastKey = reversedKeysHist.indexOf(this._lastKey);
-        reversedKeysHist.splice(indexOfReversedLastKey, 1);
-        this.keysHist = reversedKeysHist.reverse();
-
-        this._lastKey = this.keysHist[this.keysHist.length - 1];
-
-        this._cache.delete(this._lastKey);
-      }
+    if (cacheContainsKey) {
+      this._cache.delete(key);
+    } else if (exceedsCapacity) {
+      const firstValueFromCache = this._cache.keys().next().value;
+      this._cache.delete(firstValueFromCache);
     }
 
     this._cache.set(key, value);
-
-    this._lastKey = key;
-    this.keysHist.push(this._lastKey);
 
     return null;
   }
@@ -72,11 +60,5 @@ const returnedValues = [];
 });
 
 console.log(returnedValues);
-
-// actual [
-//   null, null, 1,  null,
-//   2,    null, -1, 3,
-//   4
-// ]
 
 //expected [null,null,null,1,null,-1,null,-1,3,4]

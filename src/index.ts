d533fd5385c249c77f7883951f699b5e016a0a6a
serhii <sergiychech@gmail.com>
2022-07-31 22:12:23 +0300

    lambda and function class methods

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,21 @@
 console.log('should run');
 
-class LRUCache {
-  private _capacity = 0;
-  private _cache: Map<number, number>;
-
-  constructor(capacity: number) {
-    this._capacity = capacity;
-    this._cache = new Map();
+class A {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
   }
 
-  get(key: number): number {
-    let result: number;
-    if (this._cache.has(key)) {
-      // when we call the 'get' method, push the key to
-      // the end of the queue, as we just accessed it.
-      result = this._cache.get(key);
+  getMeL = () => {
+    return this;
+  };
 
-      this._cache.delete(key);
-      this._cache.set(key, result);
-    } else {
-      result = -1;
-    }
-
-    return result;
-  }
-
-  put(key: number, value: number) {
-    const cacheContainsKey = this._cache.has(key);
-    const exceedsCapacity = this._cache.size + 1 > this._capacity;
-
-    if (cacheContainsKey) {
-      this._cache.delete(key);
-    } else if (exceedsCapacity) {
-      const firstValueFromCache = this._cache.keys().next().value;
-      this._cache.delete(firstValueFromCache);
-    }
-
-    this._cache.set(key, value);
-
-    return null;
+  getMe() {
+    return this;
   }
 }
 
-var obj = new LRUCache(2);
+const a = new A('a class');
+let g = a.getMe;
 
-const returnedValues = [];
-
-[[1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]].forEach((item) => {
-  let returnedVal: number;
-  if (item.length === 1) {
-    const [el] = item;
-    returnedVal = obj.get(el);
-  } else if (item.length === 2) {
-    const [key, val] = item;
-    returnedVal = obj.put(key, val);
-  }
-
-  returnedValues.push(returnedVal);
-});
-
-console.log(returnedValues);
-
-//expected [null,null,null,1,null,-1,null,-1,3,4]
+console.log(g.call(a, 'some arg'));

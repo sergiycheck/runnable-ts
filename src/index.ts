8a29221d43d72bbcbe78ae17bfc30891105df73d
serhii <sergiychech@gmail.com>
2022-08-01 19:33:59 +0300

    another variant of anagrams with ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 console.log('should run');
+
+declare global {
+  interface String {
+    sort(): boolean;
+  }
+}
+
+String.prototype.sort = function () {
+  //abba => aabb
+  return this.split('').sort().join('');
+};
+
+function anagrams(word, words) {
+  const sorted = word.sort();
+  return words.filter((currentWord) => {
+    return sorted === currentWord.sort();
+  });
+}
+
+export default anagrams;

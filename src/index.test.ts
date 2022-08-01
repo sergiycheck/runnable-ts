492b48e4fb3c1de956f6a4434578f4bccc995334
serhii <sergiychech@gmail.com>
2022-08-01 19:23:16 +0300

    variant of searching for anagrams with object comparision with letters count

---
+++ src/index.test.ts
@@ -0,0 +1,19 @@
+import { assert } from 'chai';
+import anagrams from './index';
+
+describe('Anagrams', function () {
+  it('should return a list of all anagrams', function () {
+    assert.deepEqual(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), [
+      'aabb',
+      'bbaa',
+    ]);
+    assert.deepEqual(
+      anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']),
+      ['carer', 'racer']
+    );
+  });
+
+  it('should return an empty array for no anagrams', function () {
+    assert.deepEqual(anagrams('laser', ['lazing', 'lazy', 'lacer']), []);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
 console.log('should run');
+
+function getObjOfWordsCount(word) {
+  return word.split('').reduce((prev, curr) => {
+    if (curr in prev) {
+      prev[curr]++;
+    } else {
+      prev[curr] = 1;
+    }
+
+    return prev;
+  }, {});
+}
+
+function compareObjects(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  if (
+    typeof obj1 !== 'object' ||
+    typeof obj2 !== 'object' ||
+    obj1 == null ||
+    obj2 == null
+  ) {
+    return false;
+  }
+
+  let keys1 = Object.keys(obj1),
+    keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let key of keys1) {
+    if (!keys2.includes(key)) return false;
+
+    if (!compareObjects(obj1[key], obj2[key])) return false;
+  }
+
+  return true;
+}
+
+export default function anagrams(word: string, words: string[]) {
+  const wordObj = getObjOfWordsCount(word);
+
+  const filteredWords = words.filter((w) => {
+    const currentWordObj = getObjOfWordsCount(w);
+    const result = compareObjects(wordObj, currentWordObj);
+
+    console.log('result ', result, ' of comparing ', wordObj, currentWordObj);
+
+    return result;
+  });
+
+  return filteredWords;
+}
+
+const result = anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']);
+
+console.log(result);

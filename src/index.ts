d4c8c5dc8d3124602a25ae88da5c7f906e2bc6b1
serhii <sergiychech@gmail.com>
2022-08-02 15:45:51 +0300

    add another two examples of mixins (gotchas)

--- src/index.ts
+++ src/index.ts
@@ -148,7 +148,62 @@ function alternativePattern() {
   }
 }
 
+function constraintsWithDecoratosAndMixins() {
+  const Pausable = (target: typeof Player) => {
+    return class Pausable extends target {
+      shouldFreeze = false;
+    };
+  };
+
+  @Pausable
+  class Player {
+    x = 0;
+    y = 0;
+  }
+
+  // the player class does not have the decorator's type merged
+  const player = new Player();
+  // player.shouldFreeze
+
+  // the runtime aspect could be manually replicated via
+  // type composition or interface merging
+
+  type FreezablePlayer = Player & { shouldFreeze: boolean };
+
+  const playerFreezable = new Player() as unknown as FreezablePlayer;
+
+  console.log(`playerFreezable.shouldFreeze `, playerFreezable.shouldFreeze);
+}
+
+function static_property_mixins() {
+  function base<T>() {
+    class Base {
+      static prop: T;
+    }
+
+    return Base;
+  }
+
+  function derived<T>() {
+    class Derived extends base<T>() {
+      static anotherProp: T;
+    }
+
+    return Derived;
+  }
+
+  class Spec extends derived<string>() {}
+
+  Spec.prop = 'my prop 1';
+  Spec.anotherProp = 'another prop';
+
+  console.log('Spec ', Spec);
+}
+
 (function main() {
   // mixinExample1();
-  alternativePattern();
+  // alternativePattern();
+  // constraintsWithDecoratosAndMixins();
+
+  static_property_mixins();
 })();

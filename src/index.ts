059d7ff3c4fe31393bf7e8321e9cb10a15f9036c
serhii <sergiychech@gmail.com>
2022-08-08 13:21:56 +0300

    add some files, changes index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 console.log('should run');
 
 (function main() {})();
+
+const a = {
+  b: 'val 1',
+  c: 'val 2',
+};
+
+const arr = [1, 3, 3];
+
+// for (let i in arr) {
+//   console.log(i);
+// }

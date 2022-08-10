64ef9bb8b6262bcb87d618ce142b9b95e885b63f
serhii <sergiychech@gmail.com>
2022-08-11 00:02:27 +0300

    class name

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ console.log('should run');
 (async function main() {
   //
 
-  setTimeout(() => console.log('timeout'));
-  const p = new Promise((resolve) => resolve('resolved'));
-  const r = await p;
-  console.log('result ', r);
+  class Abd {}
+  console.log(Abd.name);
 })();

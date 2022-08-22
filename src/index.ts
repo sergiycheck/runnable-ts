58f8c0935279a8afcf29ccb4b3c9e5e9b52b61d4
serhii <sergiychech@gmail.com>
2022-08-22 22:50:28 +0300

    when === lies

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 console.log('should run');
 
 (async function main() {
-  var adult = true;
-  // var myName = 'GlobalName';
-
-  if (adult) {
-    var myName = 'Kyle';
-    let age = 39;
-    console.log('Shhh, this is a secret!');
-  }
-
-  console.log(myName);
-  // Kyle
-
-  // console.log(age);
-  // Error!
+  NaN === NaN; // false
+  0 === -0; // true
 })();

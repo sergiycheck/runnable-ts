2f2b7773049e513d590b9b12fe9e33043db42151
serhii <sergiychech@gmail.com>
2022-08-22 23:31:37 +0300

    comparison with < > and other operators

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 console.log('should run');
 
 (async function main() {
-  var arr = ['1', '10', '100', '1000'] as any;
-  for (let i = 0; i < arr.length && arr[i] < 500; i++) {
-    // will run 3 times
-    console.log(i);
-  }
+  var x = '10';
+  var y = '9';
+  var z = 8;
+
+  x < y; // true, watch out!
 })();

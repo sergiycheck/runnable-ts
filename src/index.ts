91af8a6eb98b879b0c246d9b73968355ba7d0ef7
serhii <sergiychech@gmail.com>
2022-08-22 22:58:16 +0300

    compare with Object.is same value

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 console.log('should run');
 
 (async function main() {
-  NaN === NaN; // false
-  0 === -0; // true
+  console.log(NaN === NaN); // false
+  console.log(0 === -0); // true
+
+  console.log(Object.is(NaN, NaN));
+  console.log(Object.is(0, -0));
 })();

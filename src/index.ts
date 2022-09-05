ad3c477c2a81ce15d5d7dc8c546d40c440ef5638
serhii <sergiychech@gmail.com>
2022-09-05 18:43:04 +0300

    some changes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 console.log('should run');
 
 (async function main() {
-  var adult = true;
-
-  if (adult) {
-    var myName = 'Kyle';
-    let age = 39;
-    console.log('Shhh, this is a secret!');
-  }
-
-  console.log(myName);
-  // Kyle
-
-  // console.log(age);
-  // Error!
+  const str = 'tokenId_1';
+  const [name, num] = str.split('tokenId_');
+  console.log(name, num);
 })();

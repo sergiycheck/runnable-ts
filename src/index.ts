cb77b64650830aeec92875b73f0bdab3ec2157d2
serhii <sergiychech@gmail.com>
2022-09-16 00:24:29 +0300

    Object create

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 console.log('should run');
 
 (async function main() {
-  const str = 'tokenId_1';
-  const [name, num] = str.split('tokenId_');
-  console.log(name, num);
+  const homework = {
+    topic: 'js',
+  };
+
+  const otherHm = Object.create(homework);
+
+  console.log(otherHm.topic);
+
+  homework.topic = 'ts';
+
+  console.log(otherHm.topic);
 })();

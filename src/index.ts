e3dc18ecc171da3d0356991fe454636ac6c24297
serhii <sergiychech@gmail.com>
2022-12-14 00:11:04 +0200

    this keyword example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-(function main() {})();
+(function main() {
+  const obj = {
+    aOuter: function () {
+      const innerA = () => {
+        return this;
+      };
+
+      return innerA;
+    },
+  };
+
+  const resultWithBoundObj = obj.aOuter();
+  const passedAOuter = obj.aOuter;
+  const resultWithGlobalThis = passedAOuter();
+
+  console.log('obj', resultWithBoundObj());
+  console.log('global ', resultWithGlobalThis());
+})();

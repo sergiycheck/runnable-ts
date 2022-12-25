a46eaadc65f4d3d54ecb73eb0ec41c6bd7466edf
serhii <sergiychech@gmail.com>
2022-12-25 14:27:05 +0200

    explanation of loose equality;

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,46 @@
 (function main() {
-  console.log(afuncExp);
-  if (typeof afuncExp === 'function') afuncExp();
+  // const emptyString = '';
+  // const emptyArr = [];
 
-  console.log(afuncDecl);
-  if (typeof afuncDecl === 'function') afuncDecl();
+  // console.log(`emptyString == emptyArr `, emptyString == (emptyArr as any));
 
-  var afuncExp = function () {
-    let a = '123';
-    console.log(a);
+  // // value-comparison operations in JS
+  // //   === — strict equality (triple equals)
+  // // == — loose equality (double equals)
+  // // Object.is()
+
+  // // four equality algorithms in JS
+  // // isLooselyEqual ==
+  // // isStrictlyEqual ===
+  // // SameValue Object.is
+  // // SameValueZero
+
+  // console.log(typeof emptyArr);
+  // console.log(`emptyArr[Symbol.toPrimitive]`, emptyArr[Symbol.toPrimitive]);
+  // const valueOfResult = [].valueOf();
+  // console.log('valueOfResult', valueOfResult);
+  // console.log(typeof valueOfResult);
+  // const toStringResult = valueOfResult.toString();
+  // console.log(`toStringResult`, toStringResult);
+
+  // // same length and order of characters
+  // console.log('toStringResult == emptyString', toStringResult == emptyString);
+
+  // toPrimitive demonstration
+
+  type Hint = string | number | 'default';
+
+  const obj = {
+    [Symbol.toPrimitive](_hint: Hint) {
+      return '123';
+    },
   };
 
-  function afuncDecl() {
-    let a = 'a declaration';
-    console.log(a);
-  }
+  console.log('123' == (obj as any));
+
+  Array.prototype[Symbol.toPrimitive] = function (_hint: Hint) {
+    return 'arr';
+  };
+
+  console.log('arr' == ([] as any));
 })();

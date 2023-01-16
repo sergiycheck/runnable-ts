2ffcd25709cd33b5d998376b904e46f58b35b94f
serhii <sergiychech@gmail.com>
2023-01-16 11:48:01 +0200

    validate json strings

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,14 @@
 (function main() {
-  // const emptyString = '';
-  // const emptyArr = [];
+  const text = JSON.stringify({ 'valid-json': 'I am valid' });
+  if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').
+    replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+    replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
 
-  // console.log(`emptyString == emptyArr `, emptyString == (emptyArr as any));
+    console.log('json is valid');
 
-  // // value-comparison operations in JS
-  // //   === — strict equality (triple equals)
-  // // == — loose equality (double equals)
-  // // Object.is()
+  } else {
 
-  // // four equality algorithms in JS
-  // // isLooselyEqual ==
-  // // isStrictlyEqual ===
-  // // SameValue Object.is
-  // // SameValueZero
+    console.log('json is not valid');
 
-  // console.log(typeof emptyArr);
-  // console.log(`emptyArr[Symbol.toPrimitive]`, emptyArr[Symbol.toPrimitive]);
-  // const valueOfResult = [].valueOf();
-  // console.log('valueOfResult', valueOfResult);
-  // console.log(typeof valueOfResult);
-  // const toStringResult = valueOfResult.toString();
-  // console.log(`toStringResult`, toStringResult);
-
-  // // same length and order of characters
-  // console.log('toStringResult == emptyString', toStringResult == emptyString);
-
-  // toPrimitive demonstration
-
-  type Hint = string | number | 'default';
-
-  const obj = {
-    [Symbol.toPrimitive](_hint: Hint) {
-      return '123';
-    },
-  };
-
-  console.log('123' == (obj as any));
-
-  Array.prototype[Symbol.toPrimitive] = function (_hint: Hint) {
-    return 'arr';
-  };
-
-  console.log('arr' == ([] as any));
+  }
 })();

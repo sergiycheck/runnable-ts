49fa3b51d60d439c3e85a4cca8ad965d1174811f
serhii <sergiychech@gmail.com>
2023-01-21 13:33:21 +0200

    clousure example

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 (function main() {
-  var studentName = "Kyle";
+  const lookupStudents = (studentId: number) => {
+    const students = {
+      1: {
+        id: 1, name: 'Frank',
+      },
+      2: {
+        id: 2, name: 'John',
+      }
+    }
 
-  {
-    // @ts-ignore
-    console.log(studentName);
-    // ???
-
-    // ..
-
-    let studentName = "Suzy";
-
-    console.log(studentName);
-    // Suzy
+    return function greetStudent(greeting: string) {
+      return `${greeting} ${students[studentId].name}!`
+    }
   }
+
+  const studentsToGreet = [
+    lookupStudents(1),
+    lookupStudents(2)
+  ];
+
+  console.log(studentsToGreet[0].name, studentsToGreet[0]('Hi'));
+  console.log(studentsToGreet[1].name, studentsToGreet[1]('Hi, how are you doing'));
 })();

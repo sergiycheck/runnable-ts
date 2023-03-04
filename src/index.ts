284228028668f086ea07e5e2614b52b77599dabc
serhii <sergiychech@gmail.com>
2023-03-04 19:21:23 +0200

    sliding window technique with length of longest substring

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,3 @@
-// function lengthOfLongestSubstring(s: string): number {
-//   if (!s) return 0;
-
-//   if (s.length == 1) return 1;
-
-//   const set = new Set();
-
-//   let maxLength = 0;
-//   let left = 0;
-
-//   for (let right = 0; right < s.length; right++) {
-//     let elRight = s[right];
-//     let elLeft = s[left];
-
-//     if (!set.has(elRight)) {
-//       set.add(elRight);
-
-//       /**  ?? dont understant*/
-//       maxLength = Math.max(maxLength, right - left + 1);
-//     } else {
-//       while (elLeft !== elRight) {
-//         set.delete(elLeft);
-//         left++;
-//         elLeft = s[left];
-//       }
-//       set.delete(elLeft);
-//       left++;
-
-//       set.add(elRight);
-//     }
-//   }
-
-//   return maxLength;
-// }
-
 function lengthOfLongestSubstring(str: string): number {
   if (!str) return 0;
 

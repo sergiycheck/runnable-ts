86dc4841ef31a9f211044d320f52909ba85ed92b
serhii <sergiychech@gmail.com>
2023-03-04 18:53:29 +0200

    solution two of length of longest substring

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,56 @@
-function lengthOfLongestSubstring(s: string): number {
-  if (!s) return 0;
+// function lengthOfLongestSubstring(s: string): number {
+//   if (!s) return 0;
 
-  if (s.length == 1) return 1;
+//   if (s.length == 1) return 1;
+
+//   const set = new Set();
+
+//   let maxLength = 0;
+//   let left = 0;
+
+//   for (let right = 0; right < s.length; right++) {
+//     let elRight = s[right];
+//     let elLeft = s[left];
+
+//     if (!set.has(elRight)) {
+//       set.add(elRight);
+
+//       /**  ?? dont understant*/
+//       maxLength = Math.max(maxLength, right - left + 1);
+//     } else {
+//       while (elLeft !== elRight) {
+//         set.delete(elLeft);
+//         left++;
+//         elLeft = s[left];
+//       }
+//       set.delete(elLeft);
+//       left++;
+
+//       set.add(elRight);
+//     }
+//   }
+
+//   return maxLength;
+// }
+
+function lengthOfLongestSubstring(str: string): number {
+  if (!str) return 0;
+
+  if (str.length == 1) return 1;
 
   const set = new Set();
 
   let maxLength = 0;
   let left = 0;
 
-  for (let right = 0; right < s.length; right++) {
-    let elRight = s[right];
-    let elLeft = s[left];
-
-    if (!set.has(elRight)) {
-      set.add(elRight);
-
-      /**  ?? dont understant*/
-      maxLength = Math.max(maxLength, right - left + 1);
-    } else {
-      while (elLeft !== elRight) {
-        set.delete(elLeft);
-        left++;
-        elLeft = s[left];
-      }
-      set.delete(elLeft);
+  for (let right = 0; right < str.length; right++) {
+    while (set.has(str[right])) {
+      set.delete(str[left]);
       left++;
-
-      set.add(elRight);
     }
+
+    set.add(str[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
   }
 
   return maxLength;

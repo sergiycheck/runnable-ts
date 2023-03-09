b34559211c0454a713dea777c47464bc2785d5d1
serhii <sergiychech@gmail.com>
2023-03-09 16:16:27 +0200

    foreach with delay

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-let numbers = [1, 2, 3, 4, NaN];
+function delay() {
+  return new Promise((resolve) => setTimeout(resolve, 2000));
+}
 
-console.log(numbers.indexOf(NaN));
+async function delayedLog(item) {
+  await delay();
+  console.log(item);
+}
 
-console.log(numbers.findIndex(Number.isNaN)); // 4
+async function process(array) {
+  array.forEach(async (item) => {
+    await delayedLog(item);
+  });
 
-console.log(numbers.includes(NaN));
+  // for (let item in array) {
+  //   await delayedLog(item);
+  // }
+
+  console.log('Process completed!');
+}
+process([1, 2, 3, 5]);
+
+let end = Date.now();
+
+///Process completed!
+/// wait for 2000ms
+//1, 2, 3, 5

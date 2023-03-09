7dae989fc800527d385ea5b9510f162202dfb40e
serhii <sergiychech@gmail.com>
2023-03-09 16:48:54 +0200

    iterators for object implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-const sym1 = Symbol('one');
-const sym2 = Symbol('one');
+const obj1 = {
+  a: 1,
+  b: 2,
+  c: 3,
+  [Symbol.iterator]() {
+    const keys = Object.keys(this);
+    let i = 0;
+    return {
+      next: () => {
+        return { value: this[keys[i++]], done: i > keys.length };
+      },
+    };
+  },
+};
 
-const sym3 = Symbol.for('two');
-const sym4 = Symbol.for('two');
+const iterator1 = obj1[Symbol.iterator]();
 
-console.log(sym1 === sym2, sym3 === sym4);
-///false, true
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+
+const obj2 = {
+  a: 1,
+  b: 2,
+  c: 3,
+  [Symbol.iterator]: function* () {
+    for (let key in this) {
+      yield this[key];
+    }
+  },
+};
+
+const iterator2 = obj2[Symbol.iterator]();
+
+console.log(iterator2.next());
+console.log(iterator2.next());
+console.log(iterator2.next());
+console.log(iterator2.next());
+console.log(iterator2.next());

71a5093fa9e500c37d78d6badf08d6a0bc3b49a6
serhii <sergiychech@gmail.com>
2023-03-09 10:41:23 +0200

    circular reference

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-function lengthOfLongestSubstring(str: string): number {
-  if (!str) return 0;
+// circular reference
 
-  if (str.length == 1) return 1;
+const a = {};
+const b = a as any;
+b.a = a;
 
-  const set = new Set();
-
-  let maxLength = 0;
-  let left = 0;
-
-  for (let right = 0; right < str.length; right++) {
-    while (set.has(str[right])) {
-      set.delete(str[left]);
-      left++;
-    }
-
-    set.add(str[right]);
-    maxLength = Math.max(maxLength, right - left + 1);
-  }
-
-  return maxLength;
-}
-
-console.log(lengthOfLongestSubstring('abcabcbb'));
-console.log(lengthOfLongestSubstring('bbbbb'));
-console.log(lengthOfLongestSubstring('pwwkew'));
+console.log(JSON.stringify(b));

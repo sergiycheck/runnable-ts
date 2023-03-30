1c5bb67dee669bfb5b9b88ab5e4359eda95ccac8
serhii <sergiychech@gmail.com>
2023-03-30 12:27:58 +0300

    some js tests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
-var x = 2;
+//3
+// function MyArray() {}
+// MyArray.prototype = [];
 
-function log() {
-  console.log(x);
-  var x = 1;
-}
+// let arr = new MyArray();
+// arr.push(1, 2, 3);
+// console.log(arr.length);
 
-log();
+// //4
+// function F() {
+//   return F;
+// }
+
+// console.log(new F() instanceof F);
+// console.log(new F() instanceof Function);
+
+//8
+// f.call(null);
+
+// function f() {
+//   console.log(this);
+// }
+
+//12
+// 'use strict';
+
+// a = null + undefined;
+// alert(a);
+
+//13
+// let a = 1;
+// let b = {
+//   toString() {
+//     return '1';
+//   },
+// };
+// let c = 1;
+
+// console.log(a + b + c);
+
+//14
+// f.call(f);
+
+// function f() {
+//   console.log(this);
+// }
+
+//16
+// NaN
+// let a = X;
+// alert( a == X ); // false

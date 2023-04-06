81fa9bf5d91895e57c1c103a333b15870a45678f
serhii <sergiycheck@gmail.com>
2023-04-06 11:32:28 +0300

    changes to run all examples

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-// //1 returnings of comparisons
+//1 returnings of comparisons
 
-// console.log(1 && 2 && false);
-// console.log((1 && 2) || false);
+console.log(1 && 2 && false);
+console.log((1 && 2) || false);
 
-// // 2 TDZ
+// 2 TDZ
 
-// try {
-//   askQuestion();
-//   // ReferenceError
+try {
+  askQuestion();
+  // ReferenceError
 
-//   let studentName = 'Suzy';
+  let studentName = 'Suzy';
 
-//   function askQuestion() {
-//     console.log(`${studentName}, do you know?`);
-//   }
-// } catch (error) {
-//   console.error(error);
-// }
+  function askQuestion() {
+    console.log(`${studentName}, do you know?`);
+  }
+} catch (error) {
+  console.error(error);
+}
 
-// // 3 circular reference
+// 3 circular reference
 
-// try {
-//   const a = {};
-//   const b = a;
-//   b.a = a;
+try {
+  const a = {};
+  const b = a;
+  b.a = a;
 
-//   console.log(JSON.stringify(b));
-// } catch (error) {
-//   console.error(error);
-// }
+  console.log(JSON.stringify(b));
+} catch (error) {
+  console.error(error);
+}
 
-// //4 demonstration of this
+//4 demonstration of this
 
-// const obj = {
-//   innerArrow: () => {
-//     return this;
-//   },
-//   innerMethod: function () {
-//     return this;
-//   },
+const obj = {
+  innerArrow: () => {
+    return this;
+  },
+  innerMethod: function () {
+    return this;
+  },
 
-//   aOuter: function () {
-//     const innerA = () => {
-//       return this;
-//     };
+  aOuter: function () {
+    const innerA = () => {
+      return this;
+    };
 
-//     return innerA;
-//   },
-// };
+    return innerA;
+  },
+};
 
-// console.log('obj.innerArrow()', obj.innerArrow());
-// console.log('obj.innerMethod()', obj.innerMethod());
+console.log('obj.innerArrow()', obj.innerArrow());
+console.log('obj.innerMethod()', obj.innerMethod());
 
-// const resultWithBoundObj = obj.aOuter();
-// const passedAOuter = obj.aOuter;
-// const resultWithGlobalThis = passedAOuter();
+const resultWithBoundObj = obj.aOuter();
+const passedAOuter = obj.aOuter;
+const resultWithGlobalThis = passedAOuter();
 
-// console.log('resultWithBoundObj', resultWithBoundObj());
-// console.log('resultWithGlobalThis ', resultWithGlobalThis());
+console.log('resultWithBoundObj', resultWithBoundObj());
+console.log('resultWithGlobalThis ', resultWithGlobalThis());

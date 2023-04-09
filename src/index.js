191e5908f2ac3a5c064bd6c90150020dfe0b2046
serhii <sergiycheck@gmail.com>
2023-04-09 14:14:19 +0300

    get unique values from arr with set

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-let ingredients_list = ['noodles', { list: ['eggs', 'flour', 'water'] }];
-
-let ingredients_list_copy = Array.from(ingredients_list);
-
-console.log('ingredients_list ', ingredients_list);
-
-ingredients_list_copy[1].list = ['rice flour', 'water'];
-
-console.log('ingredients_list[1].list ', ingredients_list[1].list);
-
-console.log('ingredients_list ', ingredients_list);
-
-ingredients_list_copy[0] = 'rice noodles';
-
-console.log('ingredients_list[0] ', ingredients_list[0]);
-
-console.log('ingredients_list_copy ', ingredients_list_copy);
-
-console.log(' ingredients_list ', ingredients_list);
+const arr = [-1, -2, -1, -1, 0, 1, 2, 1, 1, 3, 3, 4, 3, 5, 6, 9, 9];
+console.log(arr);
+const set = new Set(arr);
+console.log([...set.values()]);

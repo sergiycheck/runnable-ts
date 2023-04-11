b1993db586540a95313fc90ee3006b68c815419e
serhii <sergiycheck@gmail.com>
2023-04-11 11:52:30 +0300

    quick sort repeat

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-const arr = [-1, -2, -1, -1, 0, 1, 2, 1, 1, 3, 3, 4, 3, 5, 6, 9, 9];
-console.log(arr);
-const set = new Set(arr);
-console.log([...set.values()]);
+const arr = [-1, -2, -1];
+
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let firstEl = arr.shift();
+  let centerArr = [firstEl];
+  let leftArr = [];
+  let rightArr = [];
+  while (arr.length) {
+    let currEl = arr.shift();
+    if (currEl === firstEl) {
+      centerArr.push(currEl);
+    } else if (currEl < firstEl) {
+      leftArr.push(currEl);
+    } else {
+      rightArr.push(currEl);
+    }
+  }
+
+  let leftArrSorted = quickSort(leftArr);
+  let rightArrSorted = quickSort(rightArr);
+
+  return leftArrSorted.concat(centerArr, rightArrSorted);
+}
+
+console.log(quickSort(arr));

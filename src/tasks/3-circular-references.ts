2234847c87df630af80d6a228d67a21c6bedc10d
Serhii Kuzmych <sergiycheck@gmail.com>
2023-04-23 16:17:39 +0300

    changed circular references and this example

--- src/tasks/3-circular-references.ts
+++ src/tasks/3-circular-references.ts
@@ -1,9 +1,13 @@
+console.log(JSON.stringify({ x: 5, y: 6 }));
+// Expected output: "{"x":5,"y":6}"
+
 try {
   const a = {};
   const b = a as any;
-  b.a = a;
+  b.ainner = a;
 
   console.log(JSON.stringify(b));
 } catch (error) {
+  // TypeError: Converting circular structure to JSON
   console.error(error);
 }

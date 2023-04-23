fee51c1e5f40a60a6cf10f2330e8f59a01b88aca
Serhii Kuzmych <sergiycheck@gmail.com>
2023-04-23 15:21:12 +0300

    add more examples to logical operators and tdz

--- src/tasks/1-logical-operators.ts
+++ src/tasks/1-logical-operators.ts
@@ -1,6 +1,12 @@
 console.log(1 && 2 && false);
 console.log((1 && 2) || false);
 
+const foo = null ?? 'default string';
+console.log(foo);
+
+const baz = 0 ?? 42;
+console.log(baz);
+
 // const config = {
 //   host: process.env.HOST || 3000
 // }
--- src/tasks/2-temporad-dead-zone.ts
+++ src/tasks/2-temporad-dead-zone.ts
@@ -10,3 +10,16 @@ try {
 } catch (error) {
   console.error(error);
 }
+
+try {
+  askQuestionVar();
+  // ReferenceError
+
+  var studentName = 'Suzy';
+
+  function askQuestionVar() {
+    console.log(`${studentName}, do you know?`);
+  }
+} catch (error) {
+  console.error(error);
+}

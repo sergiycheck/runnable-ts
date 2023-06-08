65711bc4550ce193a70b398fe26fcf4c093c36b8
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-06-08 19:58:52 +0300

    usage of pipeline method

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,16 @@
-import * as util from 'util';
-import * as stream from 'stream';
-import * as fs from 'fs';
-import { once } from 'events';
-import assert from 'assert';
+import { pipeline } from 'stream';
+import fs from 'fs';
+import zlib from 'zlib';
 
-const finished = util.promisify(stream.finished);
-
-async function writeIterableToFile(iterable, filePath) {
-  const writable = fs.createWriteStream(filePath, { encoding: 'utf8' });
-
-  for await (const chunk of iterable) {
-    if (!writable.write(chunk)) {
-      await once(writable, 'drain');
+pipeline(
+  fs.createReadStream('text.txt'),
+  zlib.createGzip(),
+  fs.createWriteStream('text.txt.gz'),
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed', err);
+    } else {
+      console.log('Pipeline succeeded');
     }
   }
-
-  writable.end();
-
-  await finished(writable);
-}
-
-await writeIterableToFile(['One', ' line of text.\n'], 'log.txt');
-
-assert.equal(
-  fs.readFileSync('log.txt', { encoding: 'utf8' }),
-  'One line of text.\n'
 );

ff7110465b09733b3109036b1291ef34cba8fa8b
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-06-08 19:52:43 +0300

    creating writeIterableToFile function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
+import * as util from 'util';
+import * as stream from 'stream';
+import * as fs from 'fs';
+import { once } from 'events';
 import assert from 'assert';
-import { Readable } from 'stream';
 
-import fs from 'fs';
+const finished = util.promisify(stream.finished);
 
-var readableStream = fs.createReadStream('text.txt');
-var writableStream = fs.createWriteStream('file2.txt');
+async function writeIterableToFile(iterable, filePath) {
+  const writable = fs.createWriteStream(filePath, { encoding: 'utf8' });
 
-readableStream.setEncoding('utf8');
+  for await (const chunk of iterable) {
+    if (!writable.write(chunk)) {
+      await once(writable, 'drain');
+    }
+  }
 
-readableStream.on('data', function (chunk) {
-  writableStream.write(chunk);
-});
+  writable.end();
+
+  await finished(writable);
+}
+
+await writeIterableToFile(['One', ' line of text.\n'], 'log.txt');
+
+assert.equal(
+  fs.readFileSync('log.txt', { encoding: 'utf8' }),
+  'One line of text.\n'
+);

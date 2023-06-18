17d5ceacb42d99cea3e1972120fffd5195952558
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-06-18 19:10:28 +0300

    usage of var inside functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
-function whatsTheDealHere(id, defaultID = () => id) {
-  var id;
-
-  console.log(`local variable 'id': ${id}`);
-  console.log(`parameter 'id' (closure): ${defaultID()}`);
-
-  console.log("reassigning 'id' to 5");
-  id = 5;
-
-  console.log(`local variable 'id': ${id}`);
-  console.log(`parameter 'id' (closure): ${defaultID()}`);
+function commitAction() {
+  do {
+    const commit = () => {
+      return Math.random() > 0.5
+        ? { code: Math.random() }
+        : { code: undefined };
+    };
+    let result = commit();
+    var done = result && result.code == 1;
+  } while (!done);
 }
 
-whatsTheDealHere(3);
+function getStudents() {
+  cache = {};
+
+  try {
+    // not really a block scope
+    var records = fromCache('students');
+  } catch (err) {
+    // oops, fall back to a default
+    var records: any = [];
+  }
+  return records;
+
+  // ..
+  var cache;
+  function fromCache(name: string) {
+    if (name in cache) return cache[name];
+  }
+}

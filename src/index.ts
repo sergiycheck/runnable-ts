b1e9b51199dbad877c17465072349ffbf4824645
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-07-06 16:52:03 +0300

    optinal combinator

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 export function failure() {
   return { success: false };
 }
+
 export function success(next) {
   return { success: true, next: next };
 }
 
 export function tokenMatcherBuilder(word) {
   return (tokens, i) => {
-    if (i >= tokens.length) return $.failure();
-    else if (tokens[i] == word) return $.success(i + 1);
-    else return $.failure();
+    if (i >= tokens.length) return failure();
+    else if (tokens[i] == word) return success(i + 1);
+    else return failure();
   };
 }
 
@@ -18,27 +19,49 @@ export function sequence(...parsers) {
     for (const parser of parsers) {
       const result = parser(tokens, i);
 
-      if (!result.success) return $.failure();
+      if (!result.success) return failure();
 
       i = result.next;
     }
 
-    return $.success(i);
+    return success(i);
   };
 }
 
-export const helloWorld = sequence(
+export function anyOf(...parsers) {
+  return (tokens, i) => {
+    for (const parser of parsers) {
+      const result = parser(tokens, i);
+
+      if (result.success) return result;
+    }
+
+    return failure();
+  };
+}
+
+export function optional(parser) {
+  return (tokens, i) => {
+    const result = parser(tokens, i);
+    if (result.success) return result;
+    return success(i);
+  };
+}
+
+const helloWorldSequence = sequence(
   tokenMatcherBuilder('hello'),
   tokenMatcherBuilder('world')
 );
 
-const $ = {
-  failure: failure,
-  success: success,
-  world: tokenMatcherBuilder('world'),
-  hello: tokenMatcherBuilder('hello'),
-};
+const helloOrWorldAnyOf = anyOf(
+  tokenMatcherBuilder('hello'),
+  tokenMatcherBuilder('world')
+);
 
-console.log(helloWorld(['hello', 'world'], 0));
-console.log(helloWorld(['hello', 'notworld'], 0));
-console.log(helloWorld(['nothello', 'world'], 0));
+const book = sequence(
+  optional(tokenMatcherBuilder('the')),
+  tokenMatcherBuilder('book')
+);
+
+console.log(book(['book'], 0));
+console.log(book(['the', 'book'], 0));

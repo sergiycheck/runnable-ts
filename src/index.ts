1e596470e8d0c2f04e0e16eecb28c80bff2b3eda
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-07-06 13:20:40 +0300

    hello world combined

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,40 @@ export function success(next) {
   return { success: true, next: next };
 }
 
-const $ = {
-  failure: failure,
-  success: success,
-};
-
 export function world(tokens, i) {
   if (i >= tokens.length) return $.failure();
   else if (tokens[i] == 'world') return $.success(i + 1);
   else return $.failure();
 }
 
-console.log(world(['hello'], 0));
-console.log(world(['world'], 0));
+export function hello(tokens, i) {
+  if (i >= tokens.length) {
+    return failure();
+  } else if (tokens[i] == 'hello') {
+    return success(i + 1);
+  } else {
+    return failure();
+  }
+}
+
+const $ = {
+  failure: failure,
+  success: success,
+  world: world,
+  hello: hello,
+};
+
+export function helloWorld(tokens: string[], positionOfTheFirstToken: number) {
+  let result = $.hello(tokens, positionOfTheFirstToken) as {
+    success: boolean;
+    next: number;
+  };
+
+  if (!result.success) return $.failure();
+
+  return $.world(tokens, result.next);
+}
+
+console.log(helloWorld(['hello', 'world'], 0));
+console.log(helloWorld(['hello', 'notworld'], 0));
+console.log(helloWorld(['nothello', 'world'], 0));

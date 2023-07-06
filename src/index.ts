1771796ecab5bf8c5a19d93bdeccf83ec2f311cc
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-07-06 13:15:02 +0300

    parser for world word

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,20 @@
-const newLine1 = '\\';
-const newLine2 = 'n';
-
-const whiteSpace = ' ';
-function solution(input, markers) {
-  let strippedString = '';
-
-  let isWhiteSpace = false;
-  let indexOfWhiteSpaceStart = undefined;
-
-  let toStripLine = false;
-  let stripLineFromIndex = undefined;
-
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] == whiteSpace && !toStripLine) {
-      isWhiteSpace = true;
-      indexOfWhiteSpaceStart = i;
-      continue;
-    }
-
-    if (markers.includes(input[i])) {
-      toStripLine = true;
-      stripLineFromIndex = i;
-
-      if (isWhiteSpace) {
-        input = input
-          .split('')
-          .splice(
-            indexOfWhiteSpaceStart,
-            stripLineFromIndex - indexOfWhiteSpaceStart
-          )
-          .join('');
-      }
-      continue;
-    }
-
-    if (toStripLine && input[i] == newLine1 && input[i + 1] == newLine2) {
-      input = input
-        .split('')
-        .splice(stripLineFromIndex, i - stripLineFromIndex)
-        .join('');
-      continue;
-    }
-
-    strippedString = strippedString.concat(input[i]);
-
-    isWhiteSpace = false;
-    indexOfWhiteSpaceStart = undefined;
-
-    toStripLine = false;
-    stripLineFromIndex = undefined;
-  }
-
-  return strippedString;
+export function failure() {
+  return { success: false };
+}
+export function success(next) {
+  return { success: true, next: next };
 }
 
-const parsed = solution(
-  'apples, plums % and bananas\npears\noranges !applesauce',
-  ['%', '!']
-);
-console.log(parsed);
+const $ = {
+  failure: failure,
+  success: success,
+};
+
+export function world(tokens, i) {
+  if (i >= tokens.length) return $.failure();
+  else if (tokens[i] == 'world') return $.success(i + 1);
+  else return $.failure();
+}
+
+console.log(world(['hello'], 0));
+console.log(world(['world'], 0));

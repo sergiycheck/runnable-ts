9b5c8775132b0338f1e5fb8d0ee7898a89b282cb
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-08-24 18:46:41 +0300

    feat: longest substring

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
 import fs from 'node:fs';
 import { nextTick } from 'node:process';
 
+function getLongestUniqueSubstring(text: string) {
+  let max = 0;
+  let longestSubString = '';
+  let currentSubString = text[0];
+  for (let i = 1; i < text.length; i++) {
+    if (!currentSubString.includes(text[i])) {
+      currentSubString += text[i];
+    } else {
+      if (currentSubString.length > max) {
+        max = currentSubString.length;
+        longestSubString = currentSubString;
+      }
+
+      currentSubString = text[i];
+    }
+  }
+
+  return {text, max, longestSubString};
+}
 
 function main() {
 
-  setTimeout(() => {
-    console.log('timeout ');
-  });
-
-  setImmediate(() => console.log('immediate'));
-
-  nextTick(() => console.log('nextTick'));
-
-  Promise.resolve().then(() => console.log('promise'));
-
-  fs.readFile('./package.json', (err, data) => {
-    console.log('readFile');
-    console.log(data.toString());
-  });
-  
+  console.log(getLongestUniqueSubstring('abcabcbb'));
+  console.log(getLongestUniqueSubstring('bbbbb'));
+  console.log(getLongestUniqueSubstring('pwwkew'));
 }
 
 main();

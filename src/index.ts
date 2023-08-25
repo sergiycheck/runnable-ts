7f4b59fc2d067fca280dc660e33cc344fd67026c
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-08-25 17:25:03 +0300

    feat: hard and easy solutions

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,89 @@
-import fs from 'node:fs';
-import { nextTick } from 'node:process';
+const randomInteger = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-function getLongestUniqueSubstring(text: string) {
-  let max = 0;
-  let longestSubString = '';
-  let currentSubString = text[0];
-  for (let i = 1; i < text.length; i++) {
-    if (!currentSubString.includes(text[i])) {
-      currentSubString += text[i];
-    } else {
-      if (currentSubString.length > max) {
-        max = currentSubString.length;
-        longestSubString = currentSubString;
-      }
+const randomArr1 = Array.from({ length: 10_000 }, () =>
+  randomInteger(1, 10_000)
+);
+const randomArr2 = Array.from({ length: 10_000 }, () =>
+  randomInteger(1, 10_000)
+);
 
-      currentSubString = text[i];
+function ArrayDiffOriginal(a, b) {
+  return a.filter((el) => !b.includes(el));
+}
+
+function arrayDiff(a, b) {
+  console.log('a', a);
+  console.log('b', b);
+
+  if (!a.length) return a;
+
+  const numOccurencesOriginal = a.reduce((prev, curr, index) => {
+    if (curr in prev) {
+      return {
+        ...prev,
+        [curr]: {
+          count: prev[curr].count + 1,
+          indexes: [...prev[curr].indexes, index],
+        },
+      };
+    }
+
+    return {
+      ...prev,
+      [curr]: { count: 1, indexes: [index] },
+    };
+  }, {});
+
+  console.log('numOccurencesOriginal', numOccurencesOriginal);
+
+  const newInstanceOfArr = Object.entries(numOccurencesOriginal).map((el) => [
+    el[0],
+    { ...el[1] },
+  ]);
+
+  const numOccurencesUpdated = Object.fromEntries(newInstanceOfArr);
+
+  for (let el of b) {
+    if (el in numOccurencesUpdated) {
+      numOccurencesUpdated[el].count++;
     }
   }
 
-  return {text, max, longestSubString};
+  console.log('numOccurencesUpdated', numOccurencesUpdated);
+
+  const nonUpdatedProperties = Object.entries(numOccurencesOriginal).filter(
+    (originalEl) => {
+      if (
+        numOccurencesOriginal[originalEl[0]].count ===
+        numOccurencesUpdated[originalEl[0]].count
+      )
+        return true;
+      return false;
+    }
+  );
+
+  console.log('non updated properties');
+  console.dir(nonUpdatedProperties, { depth: null });
+
+  const diffArr = [];
+  for (let el of nonUpdatedProperties) {
+    for (let index of el[1].indexes) {
+      diffArr[index] = el[0];
+    }
+  }
+
+  console.log('diffArr', diffArr);
+
+  const filteredDiffArr = diffArr.map(Number).filter((el) => el !== undefined);
+
+  console.log('filteredDiffArr', filteredDiffArr);
+
+  return filteredDiffArr;
 }
 
-function main() {
 
-  console.log(getLongestUniqueSubstring('abcabcbb'));
-  console.log(getLongestUniqueSubstring('bbbbb'));
-  console.log(getLongestUniqueSubstring('pwwkew'));
-}
+// console.log('diff 1', ArrayDiffOriginal(randomArr1, randomArr2));
 
-main();
+console.log('diff 2', arrayDiff(randomArr1, randomArr2));

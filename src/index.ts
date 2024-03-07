0b3e063565cf21c703f2928131f8020e13a04a7d
Serhii Kuzmuch <sergiycheck@gmail.com>
2024-03-07 10:26:50 +0200

    examples with promises

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,27 @@
-const keys = ['user', 'name', 'firstname'];
+// var a = Promise.resolve().then().then();
 
-const obj = {
-  user: {
-    name: {
-      firstname: 'Bob',
-      lastname: 'Smith',
-    },
-    favoritecolor: 'Light Blue',
-  },
-};
+// var b = Promise.resolve('a');
+// console.log(b);
+// b.then();
+// b.then();
 
-// user
-console.log(obj[keys[0]]);
+async function A() {
+  return 0;
+}
 
-// user.name
-console.log(obj[keys[0]][keys[1]]);
+// A()
+//   .then((res) => {
+//     throw 'err';
+//   })
+//   .catch((err) => {
+//     console.log('err in catch', err);
+//   })
+//   .then((res) => {})
+//   .finally(() => {});
 
-// user.name.firstname
-console.log(obj[keys[0]][keys[1]][keys[2]]);
-const initData =
-  'query_id=AAGpQR4ZAAAAAKlBHhkIn5Uh&user=%7B%22id%22%3A421413289%2C%22first_name%22%3A%22Serhii%22%2C%22last_name%22%3A%22Kuzmych%22%2C%22username%22%3A%22Sieroga%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1709302913&hash=d3077bfeb31e5c80a8e052999d8f18a5cac4316e52a792ba9c59040f1f244b2e';
+var p1rej = Promise.reject('err');
+var p2 = Promise.resolve('a');
+var p3 = Promise.resolve('b');
 
-const data = initData.split('&').reduce((acc, item) => {
-  const [key, value] = item.split('=');
-
-  let valueDecoded = decodeURIComponent(value);
-  try {
-    valueDecoded = JSON.parse(valueDecoded);
-  } catch (err) {
-  } finally {
-    acc[key] = valueDecoded;
-  }
-
-  return acc;
-}, {});
-
-console.log('data', data);
+Promise.all([p1rej, p2, p3]).catch((err) => console.log('err in catch', err));
+Promise.all([p2, p3]).then((res) => console.log('res in then', res));

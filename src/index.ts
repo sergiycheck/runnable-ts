b2eda9d4454ce950e9bc94500acb18ff0af6497c
Serhii Dev <sehiidev00@gmail.com>
2024-05-21 14:38:44 +0300

    feat: promise cancelation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-import { clodinaryGettingStarted } from './cloudinary/getting-started/getting-started';
+import { setTimeout } from 'node:timers/promises';
 
-clodinaryGettingStarted();
+const request = async (path: string = 'qwe') => {
+  let timeOutTime = 500;
+  let resolvedValue;
+
+  if (Math.random() >= 0.5) {
+    timeOutTime = 3000;
+    resolvedValue = new Error(`${path} something went wrong..`);
+  } else {
+    resolvedValue = `${path} I am mock event data`;
+  }
+
+  return setTimeout(timeOutTime, Promise.resolve(resolvedValue));
+};
+
+(async () => {
+  console.time('start');
+
+  Promise.race([
+    request(),
+    (async () => {
+      await setTimeout(1000);
+      return 'timeout';
+    })(),
+  ])
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  console.timeEnd('end');
+})();
